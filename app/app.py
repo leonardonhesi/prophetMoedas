92358e50644f99535611bbb2a163e2909aea51da
Leonardo Bolognesi <leonardonhesi@gmail.com>
2020-07-12 18:14:21 -0300

    Diretorio static V3

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask      import Flask, render_template, jsonify, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='/templates')
+app = Flask(__name__, 
+            static_url_path='',
+            static_folder='templates',
+            template_folder='templates'
+            )
 app.config.from_object(__name__)
-
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
