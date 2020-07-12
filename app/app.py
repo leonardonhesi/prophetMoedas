1869c8dacc655408e88958e2cd256ebf060ba589
Leonardo Bolognesi <leonardonhesi@gmail.com>
2020-07-12 18:18:05 -0300

    Diretorio static V4

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask      import Flask, render_template, jsonify, send_from_directory
+from flask      import Flask, render_template, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__, 
@@ -12,7 +12,7 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/', methods=['GET'])
 def Homepage():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 if __name__=='__main__':
    app.run()
