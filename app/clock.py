0b4dc24e4c82b3477713cfe42fe752b96d8978a9
Leonardo Bolognesi <leonardonhesi@gmail.com>
2020-07-27 19:24:54 -0300

    forca

--- app/clock.py
+++ app/clock.py
@@ -1,12 +1,14 @@
 import time
 from atualizaData                      import dolar, lme, geral
+from app                               import socketio
 from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BackgroundScheduler(daemon=True)
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=30)
 def scheduledo():
     dolar()
     lme()
     geral()
+    socketio.emit('SOCKET_UPD',  {'data': time.strftime("%A, %d. %B %Y %I:%M:%S %p")})
     return False
 sched.start()
